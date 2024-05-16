db65ccf62332c132198c42aa89064c655955ee6c
recep daban <receprd@gmail.com>
2024-05-16 15:55:48 +0300

    seperate enemy scripts to scripts folder

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,11 +1,32 @@
 extends CharacterBody2D
 
+@onready var healthbar = $Healthbar
 
 
+func _ready():
+	healthbar.init_health(Helper.health)
 
-func _on_detection_area_body_entered(body):
-	pass # Replace with function body.
+func _physics_process(delta):
+	move_and_slide()
+
+	
+func _set_health(value):
+	healthbar.health = Helper.health
+
+func die():
+	print("enemy is dying")
+	$AnimatedSprite2D.play("die")
+	#queue_free()
 
 
-func _on_detection_area_body_exited(body):
-	pass # Replace with function body.
+func _on_hurtbox_area_entered(area):
+	print("enemy got hit!")
+	if area.name == "Hitbox":
+		healthbar.health  -= area.hurt_val
+		if healthbar.health <= 0:
+			die()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "die":
+		queue_free()
